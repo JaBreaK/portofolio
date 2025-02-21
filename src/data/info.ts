4fc0470c1cea4e141b3013c5e6d6b94e65a28637
JaBreaK <114270384+JaBreaK@users.noreply.github.com>
2025-02-21 16:39:01 +0700

    Add files via upload

---
+++ src/data/info.ts
@@ -0,0 +1,63 @@
+export const info = {
+  baseUrl: "",
+  name: "Ade Tri Fauzan",
+  jobDescription: "Web Developer, Frontend Developer, React Developer",
+  about: `Saya adalah software engineer.
+   Saya menikmati menciptakan hal-hal yang ada di internet, baik itu situs web, aplikasi, atau apa pun di antaranya.
+   Saya telah belajar 3 tahun dalam HTML, CSS, SCSS, JavaScript, Bootstrap, tailwind CSS, StyledComponent, AstroJS.
+   Karena itu, Saya telah berhasil menyelesaikan beberapa proyek web dari awal.
+   Namun, saya terus belajar dan tidak pernah berhenti mengeksplorasi teknologi baru.
+   Jadi saya juga mendapat ilmu tentang Node JS, Express JS, Mongo DB, Python,
+   firebase dan React Native seiring berjalannya waktu.
+  `,
+
+  experience: [
+    {
+      name: "Bellonime",
+      location: "Indeonesia",
+      startDate: "Des 2024",
+      endDate: "Present",
+      description: [
+        "- Developing screens and UI components for the web application using AstroJS and Tailwind.",
+        "- Fixing UI issues and integrating backend APIs.",
+      ],
+    },
+  ],
+
+  education: [
+    {
+      name: "Universitas Pamulang",
+      location: "Pamulang, Tangerang Selatan",
+      startDate: "2022",
+      endDate: "Present",
+      description: [
+        "Sarjana Teknik Informatika, Fakultas Teknik, Universitas Pamulang",
+      ],
+    },
+    {
+      name: "Man 1 Tangerang",
+      location: "Tigaraksa, Tangerang",
+      startDate: "2019",
+      endDate: "2022",
+      description: ["IPA"],
+    },
+  ],
+
+  socialMedia: {
+    facebook: "https://www.facebook.com/de.adetrifauzan/",
+    twitter: "#",
+    github: "https://github.com/jabreak",
+    email: "mailto:adetrifauzan@gmail.com",
+    linkedin: "#",
+  },
+
+  projects: [
+    {
+      title: "Bellonime",
+      isFeatured: true,
+      thumbnail: "/assets/images/bellonime.png",
+      githubUrl: "https://github.com/jabreak",
+      liveUrl: "https://bellonime.web.id",
+    },
+  ],
+};
